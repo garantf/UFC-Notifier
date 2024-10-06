89e1850c43357060c2ca3efab075e7f3c2548def
Felix Garant <102917651+garantf@users.noreply.github.com>
2024-10-06 15:54:47 -0400

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
-email_user='<email_sender>'
-email_password2='<email_sender_api_password>'
+email_user= '<email_sender>'
+email_password2= '<email_sender_api_password>'
 email_receiver = '<your email or phone number email sms gateway>'
 
